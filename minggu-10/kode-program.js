5d1b202ce6305ce89aff5371b2d9cdcb7d82114e
dimasardhana <111202012431@mhs.dinus.ac.id>
2021-11-30 12:35:07 +0700

    first commite

---
+++ minggu-10/kode-program.js
@@ -0,0 +1,5 @@
+alert("Hallo, kali ini saya membuat kode program javascript eksternal!");
+console.log("Saya sedang latihan ini bosssss")
+document.write("Ini adalah javascript");
+var hasil = document.getElementById("hasil-output");
+hasil.innerHTML = 10 / 2;
